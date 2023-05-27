8af54de5150f9b87cde4a16a09b3e1f4b47674be
jameermulani <jameer.mulani@promobitech.com>
2023-05-27 18:27:39 +0530

    Annotated App.kt with @HiltAndroidApp

--- app/src/main/java/com/example/daggerhiltdemo/App.kt
+++ app/src/main/java/com/example/daggerhiltdemo/App.kt
@@ -2,7 +2,9 @@ package com.example.daggerhiltdemo
 
 import android.app.Application
 import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
